8d7303fd1c3c9120d6bbab832cef6e3670f8b3b5
Surbma <peter@surbma.hu>
2014-08-02 15:09:45 +0200

    1.11.0
    - Removed GitHub Plugin URI
    - Removed empty version control script

--- surbma-premium-wordpress.php
+++ surbma-premium-wordpress.php
@@ -3,26 +3,12 @@
 /*
 Plugin Name: Surbma - Prémium WordPress bővítmények
 Plugin URI: http://surbma.hu/wordpress-bovitmenyek/
-GitHub Plugin URI: Surbma/surbma-premium-wordpress
 Description: Hasznos bővítmények WordPress honlapokhoz.
-Version: 1.10.1
+Version: 1.11.0
 Author: Surbma
 Author URI: http://surbma.hu/
 License: GPL2
 */
-
-$pwp_version = '1.10.1';
-define( 'PWP_VERSION_KEY', 'pwp_version' );
-define( 'PWP_VERSION_NUM', $pwp_version );
-
-add_option( PWP_VERSION_KEY, PWP_VERSION_NUM );
-
-if ( get_option( PWP_VERSION_KEY ) != $pwp_version ) {
-	// Execute your upgrade logic here
-
-	// Then update the version value
-	update_option( PWP_VERSION_KEY, $pwp_version );
-}
 
 define( 'PWP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PWP_PLUGIN_URL', plugins_url( '', __FILE__ ) );
