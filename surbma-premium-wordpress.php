15f170b272b5901c80d36e875aa97f453572359f
Surbma <peter@surbma.hu>
2014-10-02 21:45:22 +0200

    1.11.3
    - Plugin menu went down to its place. No ego mode. :)

--- surbma-premium-wordpress.php
+++ surbma-premium-wordpress.php
@@ -4,7 +4,7 @@
 Plugin Name: Surbma - Prémium WordPress bővítmények
 Plugin URI: http://surbma.hu/wordpress-bovitmenyek/
 Description: Hasznos bővítmények WordPress honlapokhoz.
-Version: 1.11.2
+Version: 1.11.3
 Author: Surbma
 Author URI: http://surbma.hu/
 License: GPL2
