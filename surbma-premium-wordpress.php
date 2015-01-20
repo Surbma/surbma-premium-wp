d018801a05f229ecc051518be718a698e6457483
Surbma <peter@surbma.hu>
2015-01-20 10:24:51 +0100

    1.14.0
    - Added an extra hook for Google Tag Manager function.

--- surbma-premium-wordpress.php
+++ surbma-premium-wordpress.php
@@ -4,7 +4,7 @@
 Plugin Name: Surbma - Prémium WordPress bővítmények
 Plugin URI: http://surbma.hu/wordpress-bovitmenyek/
 Description: Hasznos bővítmények WordPress honlapokhoz.
-Version: 1.13.0
+Version: 1.14.0
 Author: Surbma
 Author URI: http://surbma.hu/
 License: GPL2
